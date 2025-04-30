1779d870b8cbf3bc65edc5c22085bcd0dc1eea48
Manzar Abbas <manzarabbas4404@gmail.com>
2025-04-30 20:36:47 +0500

    dashboard app introduced with admin panel

---
+++ dashboard/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from account.models import User
+from beach.models import Beach, BeachLocation, BeachOpeningHour, BeachImage, BeachOpeningSeason
+from booking.models import SunbedBooking, Booking
+from inventory.models import Inventory
+from payment.models import BookingPayment
+from services.models import Facility, Rule
+from sunbed.models import Sunbed
+
+
+# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
+class SunbedAdmin(admin.ModelAdmin):
+    list_display = ("id", "beach", "price")
+    # search_fields = ("beach__title", "status")
+    # list_filter = ("beach__title")
+    # readonly_fields = ("status",)
+
+
+class SunbedBookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "booking", "sunbed")
+    # search_fields = ("booking__user__username", "sunbed__id")
+    # list_filter = ("booking__booking_date", "sunbed__status")
+
+
+# Registering all models
+admin.site.register(User, UserAdmin)
+admin.site.register(SunbedBooking, SunbedBookingAdmin)
+admin.site.register(Sunbed, SunbedAdmin)
+
+admin.site.register(Beach)
+admin.site.register(BeachImage)
+admin.site.register(BeachLocation)
+admin.site.register(BeachOpeningHour)
+admin.site.register(BeachOpeningSeason)
+admin.site.register(Inventory)
+admin.site.register(Booking)
+admin.site.register(BookingPayment)
+admin.site.register(Facility)
+admin.site.register(Rule)
--- goldbrella/settings.py
+++ goldbrella/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'booking',
     'payment',
     'inventory',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
