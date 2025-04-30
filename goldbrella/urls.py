c588a967009cf8f381ed656c40e35bc6144cbd41
Manzar Abbas <manzarabbas4404@gmail.com>
2025-04-30 20:36:46 +0500

    project initiated

---
+++ goldbrella/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for goldbrella project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="GoldBrella API",
+        default_version="v1",
+        description="API documentation for GoldBrella project",
+        terms_of_service="https://www.yourwebsite.com/terms/",
+        contact=openapi.Contact(email="support@yourwebsite.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # API Endpoints
+    path('api/v1/', include(('api.v1', 'api_v1'), namespace='v1')),
+
+    # Swagger Documentation
+    path('api/docs', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+]
