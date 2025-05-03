f0e0de8d3082f1a6a2f5f755a9eca61bf44cac01
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-03 19:44:32 +0500

    locations queryset updated for dates less than today

--- booking/views/locations.py
+++ booking/views/locations.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.utils.timezone import localdate
 from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.viewsets import GenericViewSet
@@ -19,11 +20,16 @@ class BookingLocationViewSet(ListModelMixin, GenericViewSet):
         if request_user.is_superuser or request_user.is_staff:
             return BeachLocation.objects.filter(
                 Q(
-                    beaches__bookings__booked_by=request_user,
-                    beaches__bookings__user=request_user,
-                    _connector=Q.OR
+                    Q(beaches__bookings__booking_date__lt=localdate()),
+                    Q(
+                        beaches__bookings__booked_by=request_user,
+                        beaches__bookings__user=request_user,
+                        _connector=Q.OR
+                    ),
+                    _connector=Q.AND,
                 )
             ).order_by('-beaches__bookings__booking_date')
         return BeachLocation.objects.filter(
+            beaches__bookings__booking_date__lt=localdate(),
             beaches__bookings__user=request_user
         ).order_by('-beaches__bookings__booking_date')
