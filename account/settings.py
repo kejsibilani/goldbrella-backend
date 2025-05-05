2f43d1e242880a95f91e0b129032333794b125c9
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-05 12:22:22 +0500

    settings for group permissions and signals for ensuring groups added

---
+++ account/settings.py
@@ -0,0 +1,18 @@
+GUEST_USER_PERMISSION_SET = {
+    # booking permissions
+    'add_booking',
+    'view_booking',
+    'change_booking',
+    'delete_booking',
+    # payment permissions
+    'view_bookingpayment',
+}
+
+STAFF_USER_PERMISSION_SET = {
+    'add_booking',
+    'view_booking',
+    'change_booking',
+    'delete_booking',
+    # payment permissions
+    'view_bookingpayment',
+}
