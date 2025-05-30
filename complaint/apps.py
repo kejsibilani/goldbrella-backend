738abc108f5e5d503272a819d758d5f604f35c2f
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-30 12:30:25 +0500

    complaint app fixed

--- complaint/apps.py
+++ complaint/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ComplaintConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "complaint"
+
+    def ready(self):
+        try: __import__(self.name, fromlist=['signals'])
+        except ImportError: pass
