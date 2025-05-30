90847e27937a277a72e63303686a8dc7e986e5b9
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-30 21:52:43 +0500

    static and media roots added for localhost

--- goldbrella/urls.py
+++ goldbrella/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
@@ -44,3 +45,7 @@ urlpatterns = [
     # Swagger Documentation
     path('api/docs', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
