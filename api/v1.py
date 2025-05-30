8245695da1cf99de95626a9a58921652db3009f4
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-30 22:19:35 +0500

    zone urls added

--- api/v1.py
+++ api/v1.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', include(('account.urls', 'account'), namespace='account')),
     path('', include(('beach.urls', 'beach'), namespace='beach')),
     path('', include(('location.urls', 'location'), namespace='location')),
+    path('', include(('zone.urls', 'zone'), namespace='zone')),
     path('', include(('sunbed.urls', 'sunbed'), namespace='sunbed')),
     path('', include(('services.urls', 'services'), namespace='services')),
     path('', include(('invoice.urls', 'invoice'), namespace='invoice')),
