2e4ad7b4ef6012adf3888fa760c06876fb50695a
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-30 21:53:00 +0500

    location urls added

--- api/v1.py
+++ api/v1.py
@@ -5,6 +5,7 @@ app_name = 'api_v1'
 urlpatterns = [
     path('', include(('account.urls', 'account'), namespace='account')),
     path('', include(('beach.urls', 'beach'), namespace='beach')),
+    path('', include(('location.urls', 'location'), namespace='location')),
     path('', include(('sunbed.urls', 'sunbed'), namespace='sunbed')),
     path('', include(('services.urls', 'services'), namespace='services')),
     path('', include(('invoice.urls', 'invoice'), namespace='invoice')),
