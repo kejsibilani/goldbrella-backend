d81063a30864c3d20b4e0a2037864bcdf6ea8c7a
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-31 11:04:28 +0500

    notification view updated for marking read notifications

--- notification/views.py
+++ notification/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from helpers.pagination import GenericPagination
 from notification.filters import NotificationFilterSet
@@ -17,3 +20,12 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user)
+
+    @action(detail=True, methods=['patch'], url_path='mark-read')
+    def mark_read(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # update the is_read field
+        instance.is_read = True
+        instance.save()
+        # return okay response
+        return Response(status=HTTP_200_OK)
