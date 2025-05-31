0f24af4e5173220085ab811377510f762c4eb2b0
Manzar Abbas <manzarabbas4404@gmail.com>
2025-05-31 10:36:14 +0500

    permission added to complaint

--- complaint/views.py
+++ complaint/views.py
@@ -4,10 +4,12 @@ from complaint.filters import ComplaintFilterSet
 from complaint.models import Complaint
 from complaint.serializers import ComplaintSerializer
 from helpers.pagination import GenericPagination
+from helpers.permissions import CustomDjangoModelPermissions
 
 
 # Create your views here.
 class ComplaintViewSet(viewsets.ModelViewSet):
+    permission_classes = [CustomDjangoModelPermissions]
     serializer_class = ComplaintSerializer
     filterset_class = ComplaintFilterSet
     pagination_class = GenericPagination
