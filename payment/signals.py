1634f1b55da540035858a54a45b6f9a2444ecb97
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-05 00:31:24 +0500

    payment signals removed

--- payment/signals.py
+++ payment/signals.py
@@ -1,29 +1,17 @@
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
-from rest_framework.exceptions import ValidationError
-
-from booking.choices import BookingStatusChoices
-from payment.choices import PaymentStatusChoices
-from payment.models import BookingPayment
-
-
-@receiver(pre_save, sender=BookingPayment)
-def validate_booking_payment(instance, **kwargs):
-    if instance.paid_amount > instance.total_amount:
-        raise ValidationError(
-            {'detail':"Paid amount cannot be greater than total amount."}
-        )
-    return
-
-@receiver(post_save, sender=BookingPayment)
-def set_booking_status(sender, instance, **kwargs):
-    latest_instance = sender.objects.filter(
-        booking=instance.booking
-    ).order_by('updated').last()
-    if latest_instance.payment_status == PaymentStatusChoices.PENDING.value:
-        instance.booking.status = BookingStatusChoices.PENDING.value
-    elif latest_instance.payment_status == PaymentStatusChoices.CONFIRMED.value:
-        instance.booking.status = BookingStatusChoices.CONFIRMED.value
-    else:
-        instance.booking.status = BookingStatusChoices.CANCELLED.value
-    instance.booking.save()
+# from django.dispatch import receiver
+# from payments.signals import status_changed
+# from payments import PaymentStatus
+#
+# from booking.choices import BookingStatusChoices
+#
+# @receiver(status_changed)
+# def on_payment_status_changed(sender, instance, **kwargs):
+#     if instance.status == PaymentStatus.CONFIRMED:
+#         # mark booking confirmed
+#         booking = instance.booking
+#         booking.status = BookingStatusChoices.CONFIRMED.value
+#         booking.save()
+#
+#         # # create invoice if not existing
+#         # if not hasattr(booking, 'invoice'):
+#         #     generate_invoice(booking)
