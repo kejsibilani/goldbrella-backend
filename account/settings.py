8278f7618124436a1b3c35cbd552cfe39db9a938
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-05 00:45:33 +0500

    accounts permissions updated

--- account/settings.py
+++ account/settings.py
@@ -1,11 +1,49 @@
+from helpers.short_func import crud
+
+ADMIN_USER_PERMISSION_SET = {
+    # location permissions
+    *crud('location'),
+    # service permissions
+    *crud('facility'),
+    *crud('rule'),
+    # beach permissions
+    *crud('beach'),
+    *crud('beachimage'),
+    *crud('beachopeninghour'),
+    *crud('beachopeningseason'),
+    # inventory permissions
+    *crud('inventoryitem'),
+    # sunbed permissions
+    *crud('sunbed'),
+    # zone permissions
+    *crud('zone'),
+    # user permissions
+    *crud('user'),
+    *crud('shift'),
+    # booking permissions
+    *crud('booking'),
+    *crud('bookinginvoice'),
+    *crud('bookingpayment'),
+    # complaint permissions
+    *crud('complaint'),
+    *crud('review'),
+    # notification permission
+    *crud('notification'),
+}
+
 GUEST_USER_PERMISSION_SET = {
     # booking permissions
     'add_booking',
     'view_booking',
     'change_booking',
     'delete_booking',
-    # payment permissions
-    'view_bookingpayment',
+}
+
+SUPERVISOR_USER_PERMISSION_SET = {
+    'add_booking',
+    'view_booking',
+    'change_booking',
+    'delete_booking',
 }
 
 STAFF_USER_PERMISSION_SET = {
@@ -13,6 +51,4 @@ STAFF_USER_PERMISSION_SET = {
     'view_booking',
     'change_booking',
     'delete_booking',
-    # payment permissions
-    'view_bookingpayment',
 }
