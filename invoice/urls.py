776658dabd6694c52ae043cbf562a269efa8d1bf
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-05 00:36:17 +0500

    invoice urls added

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,8 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from invoice.serializers import BookingInvoiceSerializer
+
 app_name = 'invoice'
 
-urlpatterns = [
-    # path('<int:booking_pk>/pay/', pay_for_booking, name='pay-for-booking'),
-    # path('<int:booking_pk>/pay/success/', PaymentSuccessView.as_view(), name='payment-success'),
-    # path('<int:booking_pk>/pay/failure/', PaymentFailureView.as_view(), name='payment-failure'),
-    # path('<int:booking_pk>/invoice/', download_invoice, name='download-invoice'),
-]
+router = DefaultRouter(trailing_slash=False)
+router.register(r'invoices', BookingInvoiceSerializer, basename='invoice')
+
+
+urlpatterns = router.urls
