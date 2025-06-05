af08a7062229c720d5588bbd4cab4524fbf15ca0
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-06 01:52:23 +0500

    complaint urls bug fixed

--- complaint/urls.py
+++ complaint/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from complaint.views import ComplaintViewSet
 
-app_name = "complaint",
+app_name = "complaint"
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'complaints', ComplaintViewSet, basename='complaint')
