8c2ac8d3558d6aa36c6fee8064e99bfd3a699faa
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-05 16:27:10 +0500

    booking status update on invoice status

--- invoice/signals.py
+++ invoice/signals.py
@@ -4,8 +4,7 @@ from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from booking.models import Booking
-from invoice.choices import PaymentMethodChoices
+from booking.choices import BookingStatusChoices
 from invoice.choices import PaymentStatusChoices
 from invoice.models import BookingInvoice
 
@@ -21,12 +20,15 @@ def update_invoice_status(instance, **kwargs):
     return
 
 
-@receiver(post_save, sender=Booking)
-def create_invoice_for_booking(instance, created, **kwargs):
-    if created: BookingInvoice.objects.get_or_create(
-        booking=instance,
-        defaults={
-            'payment_method': PaymentMethodChoices.STRIPE.value,
-            'paid_amount': 0,
-        }
-    )
+@receiver(post_save, sender=BookingInvoice)
+def update_booking_status(instance, **kwargs):
+    if instance.payment_status == PaymentStatusChoices.PAID.value:
+        setattr(instance.booking, 'status', BookingStatusChoices.CONFIRMED.value)
+    elif instance.payment_status == PaymentStatusChoices.PARTIAL_PAID.value:
+        setattr(instance.booking, 'status', BookingStatusChoices.RESERVED.value)
+    else:
+        if instance.booking.booked_by.has_role('guest'):
+            setattr(instance.booking, "status", BookingStatusChoices.PARTIAL_RESERVED.value)
+        else:
+            setattr(instance.booking, "status", BookingStatusChoices.RESERVED.value)
+    instance.booking.save()
