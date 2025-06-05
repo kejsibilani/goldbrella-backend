c56296e042038640e9f4a1f793c4f85b321ceb23
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-05 12:33:29 +0500

    celery bug fixed

--- goldbrella/celery.py
+++ goldbrella/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldbrella.settings')
 
 app = Celery('goldbrella')
 app.config_from_object('django.conf:settings', namespace='CELERY')
