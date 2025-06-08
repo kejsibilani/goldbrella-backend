388e4a0e2ff53f797cfd8192df1f8488d415a72f
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-08 21:05:31 +0500

    logo added to system info

--- mailer/system.py
+++ mailer/system.py
@@ -2,6 +2,7 @@ import os
 
 system_info = dict(
     name=os.getenv('COMPANY_NAME'),
+    logo=os.getenv('COMPANY_LOGO'),
     email=os.getenv('COMPANY_EMAIL'),
     website=os.getenv('COMPANY_WEBSITE'),
     phone_number=os.getenv('COMPANY_PHONE_NUMBER'),
