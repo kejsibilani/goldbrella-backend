ca7f14648f8433f63ee501d85bde250dc7db3417
Manzar Abbas <manzarabbas4404@gmail.com>
2025-06-09 01:49:09 +0500

    redirect views added

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,9 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from payment.views import BookingPaymentReadViewSet
+from payment.views.redirect import BookingFailureView
+from payment.views.redirect import BookingSuccessView
 
 app_name = 'payment'
 
@@ -8,4 +11,9 @@ router = DefaultRouter(trailing_slash=False)
 router.register(r'bookings/payments', BookingPaymentReadViewSet, basename='read-booking-payment')
 
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('booking-success/<int:pk>', BookingSuccessView.as_view(), name='payment-success'),
+    path('booking-failure/<int:pk>', BookingFailureView.as_view(), name='payment-failure'),
+]
+
+urlpatterns += router.urls
